44295ad347373944e740242cd2c27804cd93081b
Ahmed TAILOULOUTE <ahmed.tailouloute@gmail.com>
2020-11-07 16:33:05 +0100

    Rename generated binary

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ docker build -t csv2xlsx_image --build-arg ARCH=amd64 .
 docker run -i --rm -v $(pwd):/home csv2xlsx_image /bin/sh << CMD
 cd /home
 pyinstaller --onefile csv2xlsx.py
-mv dist/csv2xlsx dist/csv2xlsx_amd64
+mv dist/csv2xlsx dist/csv2xlsx_linux_amd64
 CMD
 
 # post_clean
